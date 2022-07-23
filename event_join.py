8998f71310826e95f9ddeea9f96ec06af27bcee7
yoonjaehee <46480817+yoonjaehee@users.noreply.github.com>
2022-07-23 22:17:57 +0900

    boring selenium...
    apply friend's university class macro...

--- event_join.py
+++ event_join.py
@@ -1,25 +1,38 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-driver = webdriver.Chrome("C://chromedriver")
-
-driver.get("http://www.megastudy.net/teacher_v2/t_promotion/202101_pr/0118_megabori/main.asp")
-driver.execute_script("fncShowLogin()")
-driver.implicitly_wait(3)
-time.sleep(1)
-driver.find_element_by_id("loginidly").send_keys("id")
-driver.implicitly_wait(3)
-time.sleep(1)
-driver.find_element_by_id("loginpwly").send_keys("password")
-driver.implicitly_wait(3)
-time.sleep(1)
-driver.execute_script("goLoginly()")
-driver.implicitly_wait(3)
-time.sleep(1)
-while(1):
-    try:
-        driver.execute_script("fncAppJoin('2021-01-24')")
-        driver.switch_to.alert.accept()
-    except:
-        pass
-##using pypy
+from multiprocessing import Process
+def crawling():
+    driver = webdriver.Chrome("C://chromedriver")
+    driver.get("https://kiusugang.kiu.ac.kr/sookang/login_kiujs_lp.jsp")
+    driver.find_element_by_name("userID").send_keys("")
+    driver.implicitly_wait(3)
+    time.sleep(1)
+    driver.find_element_by_name("userPWD").send_keys("")
+    driver.implicitly_wait(3)
+    time.sleep(1)
+    driver.execute_script("arguments[0].click();",driver.find_element_by_xpath('//input[@type="image"][@src="/sookang/images/btn_login.gif"]'))
+    driver.implicitly_wait(3)
+    time.sleep(1)
+    driver.switch_to.frame("fBody")
+    driver.find_element_by_xpath("/html/body/div[3]/table[2]/tbody/tr/td[2]/table[1]/tbody/tr[1]/td[2]/input").send_keys("S050160")
+    driver.find_element_by_id("id_aType02").send_keys("02")
+    driver.find_element_by_id("btnShowSbjList").click()
+    driver.switch_to.frame("sbjPro_01")
+    time.sleep(1)
+    driver.find_element_by_xpath("/html/body/div/div[3]/div[3]/div/table/tbody/tr[2]/td[1]/button").click()
+    while(1):
+        try:
+            time.sleep(0.2)
+            driver.switch_to.alert.accept()
+            time.sleep(0.2)
+            driver.switch_to.alert.accept()
+            driver.find_element_by_xpath("/html/body/div/div[3]/div[3]/div/table/tbody/tr[2]/td[1]/button").send_keys(Keys.ENTER)
+        except:
+            pass
+    
+if __name__=='__main__':
+    p1 = Process(target=crawling) #함수 1을 위한 프로세스
+    p1.start()
+    
+    
